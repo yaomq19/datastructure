72fbfef16fb3bfd13e544a0a0d63bf7604e83c14
yaomqlaptop <123456>
2023-02-16 20:49:05 +0800

    新增了lazy字段

--- segmentTree.h
+++ segmentTree.h
@@ -51,7 +51,105 @@ class SegmentTree
         int mid = (l+r)>>1;
         if(t<=mid){
             return query(ind_tree*2,l,mid,s,t);
-        }else if{
+        }else if(s>mid){
+            return query(ind_tree*2+1,mid+1,r,s,t);
+        }else{
+            return query(ind_tree*2,l,mid,s,mid)
+            + query(ind_tree*2+1,mid+1,r,mid+1,t);
+        }
+    }
+};
+struct Operation
+{
+    int order;
+    char op;
+    int v;
+    Operation(int a,char b,int c):order(a),op(b),v(c){}
+};
+bool mycmp(const Operation& a,const Operation&b)
+{
+    return a.order<b.order;
+}
+class SegmentTreeWithLazy
+{
+    public:
+    int *tree;
+    vector<Operation> *lazy;
+    int *data;
+    SegmentTree(int* a,int n)
+    {
+        data = a;
+        tree = new int[4*n+1];
+        lazy = new vector<Operation>[4*n+1];
+        int left = 0;
+        int right = n-1;
+        build(1,left,right);
+    }
+    ~SegmentTree()
+    {
+        delete[] tree;
+    }
+    int build(int ind_seg,int left,int right)
+    {
+        if(left == right)
+        {
+            tree[ind_seg] = data[left];
+            return tree[ind_seg];
+        }
+        int mid = (left+right) >>1;
+        tree[ind_seg] = build(ind_seg * 2 ,left,mid)
+        + build(ind_seg * 2 + 1,mid+1,right);
+        return tree[ind_seg];
+    }
+    //线段树中下标为ind_tree的结点维护[l,r]的区间的和值
+    void update(int ord,int ind_tree,int l,int r,int s,int t,int value,char op)
+    {
+        if(l==s&&r==t)
+        {
+            lazy[ind_tree].push_back(Operation(ord,op,value));
+            return;
+        }
+        int mid = (l+r)>>1;
+        if(t<=mid){
+            update(ord,ind_tree*2,l,mid,s,t,value,op);
+        }else if(s>mid){
+            update(ord,ind_tree*2+1,mid+1,r,s,t,value,op);
+        }else {
+            update(ord,ind_tree*2,l,mid,s,t,value,op);
+            update(ord,ind_tree*2+1,mid+1,r,s,t,value,op);
+        }
+    }
+    int query(int ind_tree,int l,int r,int s,int t)
+    {
+        if(l==s && r ==t){
+            //handle and change tree[ind_tree];
+            sort(lazy[ind_tree].begin(),lazy[ind_tree].end(),mycmp);
+            for(auto &it:lazy[ind_tree])
+            {
+                if(it.op=='+'){
+                    tree[ind_tree]+=it.v;
+                }else if(it.op=='*'){
+                    tree[ind_tree]*=it.v;
+                }
+            }
+            lazy[ind_tree].clear();
+            return tree[ind_tree];
+        }
+        int mid = (l+r)>>1;
+        lazy[ind_tree*2].insert(
+                lazy[ind_tree*2].end(),
+                lazy[ind_tree].begin(),
+                lazy[ind_tree].end()
+            );
+            lazy[ind_tree*2+1].insert(
+                lazy[ind_tree*2+1].end(),
+                lazy[ind_tree].begin(),
+                lazy[ind_tree].end()
+            );
+            lazy[ind_tree].clear();
+        if(t<=mid){
+            return query(ind_tree*2,l,mid,s,t);
+        }else if(s>mid){
             return query(ind_tree*2+1,mid+1,r,s,t);
         }else{
             return query(ind_tree*2,l,mid,s,mid)
